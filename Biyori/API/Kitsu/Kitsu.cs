3bdae258b9135d45fb08993ceb658a904725c40b
Venipa <admin@venipa.net>
2019-08-11 20:37:05 +0200

    :sparkles: kitsu api wrapper wip

---
+++ Biyori/API/Kitsu/Kitsu.cs
@@ -0,0 +1,68 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biyori.API.Kitsu
+{
+
+    public class Kitsu
+    {
+        private const string BaseURL = "https://kitsu.io/api/edge";
+        private RestClient getClient()
+        {
+            var client = new RestClient(BaseURL);
+            client
+                .AddDefaultHeader("Accept", "application/vnd.api+json")
+                .AddDefaultHeader("Content-Type", "application/vnd.api+json");
+            return client;
+        }
+
+        public Kitsu()
+        {
+
+        }
+        /// <summary>
+        /// -
+        /// </summary>
+        /// <param name="loginUser">Email or Username</param>
+        /// <param name="Password">Password or API Key</param>
+        public void InitializeUserClient(string loginUser, string Password)
+        {
+
+        }
+        public async Task<KitsuPaginationModel<KitsuDataModel>> GetAnimeByBulkId(params int[] animeId)
+        {
+            if (animeId.Length == 0)
+            {
+                return null;
+            }
+            var rr = new RestRequest("anime", Method.GET)
+                .AddQueryParameter("page[limit]", "20")
+                .AddQueryParameter("filter[id]", string.Join(",", animeId.Distinct().Select(x => x.ToString())));
+            var client = this.getClient();
+            var response = await client.ExecuteTaskAsync<KitsuPaginationModel<KitsuDataModel>>(rr);
+            return response.Data;
+        }
+        public async Task<KitsuDataModel> GetAnimeById(int animeId)
+        {
+            var rr = new RestRequest("anime", Method.GET)
+                .AddQueryParameter("page[limit]", "1")
+                .AddQueryParameter("filter[id]", animeId.ToString());
+            var client = this.getClient();
+            var response = await client.ExecuteTaskAsync<KitsuPaginationModel<KitsuDataModel>>(rr);
+            return response.Data?.Data?.FirstOrDefault();
+        }
+        public async Task<KitsuPaginationModel<KitsuDataModel>> SearchByTitle(string searchQuery, int perPage = 10)
+        {
+            var rr = new RestRequest("anime", Method.GET)
+                .AddQueryParameter("page[limit]", perPage.ToString())
+                .AddQueryParameter("filter[text]", searchQuery);
+            var client = this.getClient();
+            var response = await client.ExecuteTaskAsync<KitsuPaginationModel<KitsuDataModel>>(rr);
+            return response.Data;
+        }
+    }
+}
---
+++ Biyori/API/Kitsu/KitsuPaginationModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biyori.API.Kitsu
+{
+    public class KitsuPaginationModel<T> where T : KitsuDataModel
+    {
+        [JsonProperty("meta")]
+        public KitsuMetaModel Meta { get; set; }
+        [JsonProperty("data")]
+        public IEnumerable<T> Data { get; set; } = new List<T>();
+        [JsonProperty("links")]
+        public KitsuLinkModel Links { get; set; }
+    }
+    public class KitsuDataModel { }
+    public class KitsuMetaModel
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; } = 0;
+    }
+    public class KitsuLinkModel
+    {
+        [JsonProperty("first")]
+        public string FirstPage { get; set; }
+        [JsonProperty("previous")]
+        public string PreviousPage { get; set; }
+        [JsonProperty("next")]
+        public string NextPage { get; set; }
+        [JsonProperty("last")]
+        public string LastPage { get; set; }
+    }
+}
